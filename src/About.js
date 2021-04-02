d5bbac8d0d8d5cc16f6fba688ff95c47a8c600db
Blankeos <carloantonioct@gmail.com>
2021-04-02 23:55:29 +0800

    Router for gh pages setup

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div>Hello World</div>;
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h3 className="text-2xl">Hello World</h3>
+      </div>
+
+      <Switch>
+        <Route path="/about">
+          <About></About>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
