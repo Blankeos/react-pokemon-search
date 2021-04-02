28073bfec5be75aa4c8acf727051b047564071d2
Blankeos <carloantonioct@gmail.com>
2021-04-03 00:55:03 +0800

    Fetch API to PokeAPI and pass data into Home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import "./App.css";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./About";
+import Home from "./Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        const results = data.results.map((pokemon, idx) => {
+          return { ...pokemon, idx: idx + 1 };
+        });
+        setPokemon({ ...data, results });
+      });
+  }, []);
+
+  console.log(pokemon);
+
   return (
     <Router>
       <div className="App">
         <h3 className="text-2xl">Hello World</h3>
+        {pokemon && <Home prop={pokemon.results} />}
       </div>
 
       <Switch>
-        <Route path="/about">
+        <Route path="/about/:slug">
           <About></About>
         </Route>
       </Switch>
