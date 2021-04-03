8144508059864de2400557c4a23382f39ea94031
Blankeos <carloantonioct@gmail.com>
2021-04-03 16:40:14 +0800

    Styled Pokemon Picker header and pokemon list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import { useEffect, useState } from "react";
@@ -22,14 +22,22 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <h3 className="text-2xl">Hello World</h3>
-        {pokemon && <Home prop={pokemon.results} />}
+      <div className="p-14">
+        <div className="flex flex-col items-center">
+          <Link to="/">
+            <header className="text-4xl font-bold text-yellow-500">
+              Pokemon Picker
+            </header>
+          </Link>
+        </div>
       </div>
 
       <Switch>
         <Route path="/about/:slug">
           <About></About>
+        </Route>
+        <Route exact path="/">
+          {pokemon && <Home prop={pokemon.results} />}
         </Route>
       </Switch>
     </Router>
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Home({ prop: results }) {
   return (
-    <div>
+    <div className="mt-10 p-4 flex flex-wrap">
       {results &&
         results.map((val) => {
-          return <div key={val.idx}>{val.name}</div>;
+          return (
+            <div className="ml-4 text-2xl text-blue-400">
+              <Link to={`/about/${val.idx}`} key={val.idx}>
+                {val.name}
+              </Link>
+            </div>
+          );
         })}
     </div>
   );
